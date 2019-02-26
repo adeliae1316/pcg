ebcd14879a21fd64edb942910af64f7d87e2cc66
adeliae1316 <c011521662@edu.teu.ac.jp>
2019-02-26 09:57:02 +0900

    add status section and change circle to icon

--- pokeca.js
+++ pokeca.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function() {
   let prev = 0, times = 0, count_num = 1;
   let count_area = document.getElementById("count");
-  let screen = document.getElementById("screen");
+  let coins_screen = document.getElementById("coins_screen");
   count_area.addEventListener("click", function(){
     count_num = 0;
     count_area.innerText = count_num;
-    screen.innerText = "";
+    coins_screen.innerText = "";
   }, false)
   document.body.addEventListener("click", function(event) {
     // if (event.target.tagName == "BUTTON") {
-    console.log(event.target.className);
+    // console.log(event.target.className);
     if(event.target.className.indexOf("times") != -1){
       times = event.target.value;
       if(times == prev){
@@ -23,11 +23,11 @@ window.addEventListener("load", function() {
       for (let i = 0; i < times; i++) {
         let sides = Math.floor((Math.random() * 1000) % 2);
         if (sides == 0) {
-          out_str += "○ ";
+          out_str += " panorama_fish_eye ";
         } else {
-          out_str += "● ";
+          out_str += " block ";
         }
-        screen.innerText = out_str;
+        coins_screen.innerText = out_str;
       }
     }
     prev = times;
