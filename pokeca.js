3c8d2f409c9275cdfd938753c77469bb080a2b1a
adeliae1316 <c011521662@edu.teu.ac.jp>
2019-02-26 15:43:45 +0900

    add change_grayscale function

--- pokeca.js
+++ pokeca.js
@@ -5,20 +5,45 @@ window.addEventListener("DOMContentLoaded", function() {
   let count_area = document.getElementById("count");
   let coins_screen = document.getElementById("coins-screen");
 
+  // Get Ids of Status
+  let poison = document.getElementById("poison");
+  let burn = document.getElementById("burn");
+  let sleep = document.getElementById("sleep");
+  let paralysis = document.getElementById("paralysis");
+  let confusion = document.getElementById("confusion");
 
+  // Init Modals of Materialize
   let elems = document.querySelectorAll('.modal');
   let instances = M.Modal.init(elems);
 
-  console.log(instances);
-
-  count_area.addEventListener("click", function() {
-    count_num = 0;
-    count_area.innerText = count_num;
-    coins_screen.innerText = "";
-  }, false);
   document.body.addEventListener("click", function(event) {
-    // if (event.target.tagName == "BUTTON") {
-    // console.log(event.target.className);
+    // console.log(event.target);
+
+    switch(event.target.id){
+      case "ok-poison":
+      change_grayscale(poison);
+      break;
+      case "ok-burn":
+      change_grayscale(burn);
+      break;
+      case "ok-sleep":
+      change_grayscale(sleep);
+      break;
+      case "ok-paralysis":
+      change_grayscale(paralysis);
+      break;
+      case "ok-confusion":
+      change_grayscale(confusion);
+      break;
+    }
+
+    // Init count_area and coins_screen
+    if(event.target == count_area){
+      count_num = 0;
+      count_area.innerText = count_num;
+      coins_screen.innerText = "";
+    }
+
     if (event.target.className.indexOf("times") != -1) {
       times = event.target.value;
       if (times == prev) {
@@ -41,5 +66,16 @@ window.addEventListener("DOMContentLoaded", function() {
       prev = times;
     }
   }, false);
+
+  function change_grayscale(element){
+    if(element.className.indexOf("disable-gray") == -1){
+      element.className += " disable-gray";
+      console.log("no");
+    } else {
+      let temp = element.className.replace("disable-gray", "");
+      element.className = temp;
+      console.log("yes");
+    }
+  }
 
 }, false);
